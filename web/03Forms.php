8689be411c92508392c5542d48d6736f4cda4c80
david short <sho13014@byui.edu>
2018-05-10 19:50:17 -0600

    added teach03

--- web/03Forms.php
+++ web/03Forms.php
@@ -1,6 +1,6 @@
 <html>
 <body>
-<form action="03Response.php" method="POST">
+<form action="03Response.php" method="post">
 
 <div>Name: <input name="name" type="text"></input></div>
 <div>Email: <input name="email" type="text"></input></div>
@@ -8,13 +8,13 @@
 <div><input name="major" type="radio" value="Computer Science">Web Design and Development</div>
 <div><input name="major" type="radio" value="Computer Science">Computer Information Technology</div>
 <div><input name="major" type="radio" value="Computer Science">Computer Engineering</div>
-<input name="continent" type="checkbox" value="North America">North America</input>
-<input name="continent" type="checkbox" value="South America">South America</input>
-<input name="continent" type="checkbox" value="Europe">Europe</input>
-<input name="continent" type="checkbox" value="Asia">Asia</input>
-<input name="continent" type="checkbox" value="Australia">Australia</input>
-<input name="continent" type="checkbox" value="Africa">Africa</input>
-<input name="continent" type="checkbox" value="Antarctica">Antarctica</input>
+<input name="continent[]" type="checkbox" value="North America">North America</input>
+<input name="continent[]" type="checkbox" value="South America">South America</input>
+<input name="continent[]" type="checkbox" value="Europe">Europe</input>
+<input name="continent[]" type="checkbox" value="Asia">Asia</input>
+<input name="continent[]" type="checkbox" value="Australia">Australia</input>
+<input name="continent[]" type="checkbox" value="Africa">Africa</input>
+<input name="continent[]" type="checkbox" value="Antarctica">Antarctica</input>
 
 <div><input name="comment" type="textarea"></input></div>
 
