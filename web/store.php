a7e10c39786ef8caf958b7c8b460a2e513414f2a
david short <sho13014@byui.edu>
2018-05-12 21:36:48 -0600

    w03 ponder

--- web/store.php
+++ web/store.php
@@ -1,6 +1,6 @@
 
 
-
+<!DOCTYPE html>
 <html>
 <head>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -11,35 +11,16 @@
   <title>Store</title>
 </head>
 <body>
-  <!-- <?php
-  // $items = array("Dagger" => "2", "Hook hand" => "10", "Light Mace" => "5",
-  // "Longspear" => "5", "Light Crossbow" => "35");
 
-   ?> -->
   <div class="topnav">
     <a href="home.html">Home</a>
     <a href="assignments.html">Assignments</a>
     <a href="cart.php">Your Cart</a>
   </div>
   <div>
-    <!-- <table>
-      <tr>
-        <th>Item</th>
-        <th>Price</th>
-      </tr>
-      <?php
-      // foreach ($items as $key => $value) {
-      //   echo "<tr>";
-      //   echo "<td>" . $key . "</td>";
-      //   echo "<td>" . $value . "gp </td>"
-      //   echo '<button type="button">Add</button>'
-      //   echo "</tr>"
-      }
-       ?>
-    </table> -->
+  
   </div>
 
-<footer>
-</footer>
+
 </body>
 </html>
