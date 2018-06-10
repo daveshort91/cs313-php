13a776058a0353d45bfdaae1793e006d212622d8
david short <sho13014@byui.edu>
2018-06-09 20:26:34 -0600

    polishing project 1

--- web/connectdb.php
+++ web/connectdb.php
@@ -8,14 +8,15 @@
   			$dbUrl = "postgres://coojwhvzpsggyt:ac510fc22c734d804f00cd3d140cd581169d8afdc2a92e8d4838b9c26817e35d@ec2-54-204-39-46.compute-1.amazonaws.com:5432/ddablnlhhmt81l";
 
   		}
-      $dbopts = parse_url($dbUrl);
-  		$dbHost = $dbopts["ec2-54-204-39-46.compute-1.amazonaws.com"];
-  		$dbPort = $dbopts["5432"];
-  		$dbUser = $dbopts["coojwhvzpsggyt"];
-  		$dbPassword = $dbopts["ac510fc22c734d804f00cd3d140cd581169d8afdc2a92e8d4838b9c26817e35d"];
-  		$dbName = ltrim($dbopts["ddablnlhhmt81l"],'/');
+      // $dbopts = parse_url($dbUrl);
+  		// $dbHost = $dbopts["ec2-54-204-39-46.compute-1.amazonaws.com"];
+  		// $dbPort = $dbopts["5432"];
+  		// $dbUser = $dbopts["coojwhvzpsggyt"];
+  		// $dbPassword = $dbopts["ac510fc22c734d804f00cd3d140cd581169d8afdc2a92e8d4838b9c26817e35d"];
+  		// $dbName = ltrim($dbopts["ddablnlhhmt81l"],'/');
 
-      $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+      // $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+      $db = new PDO("$dbUrl", $dbUrl);
 
 
       $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
